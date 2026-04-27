d15bdc8553b3822501734d839df221ca78e34a17
aisha278 <aisha@mohban.com>
2026-04-27 00:53:37 -0400

    Refactor form field handling in app.py

--- app.py
+++ app.py
@@ -13,16 +13,34 @@ def fill_standby_guardian():
         data = request.json or {}
 
         field_data = {
-            "Your Name(s)": data.get("parent_names", ""),
-            "Name of Standby Guardian": data.get("standby_guardian_name", ""),
-            "Name of Alternate Standby Guardian": data.get("alternate_guardian_name", ""),
+            "Your Name 1": data.get("parent_names", ""),
+            "Address 1": data.get("parent_address", ""),
+            "Telephone Number 1": data.get("parent_phone", ""),
+            "E-mail 1": data.get("parent_email", ""),
 
-            "Name of Person with Parental Rights 2": data.get("other_parent_name", "NONE"),
-            "Relationship to Minor child 2": data.get("other_parent_relationship", "N/A"),
+            "Name of Standby Guardian": data.get("standby_guardian_name", ""),
+            "Address 2": data.get("standby_guardian_address", ""),
+            "Telephone Number 2": data.get("standby_guardian_phone", ""),
+            "Email 2": data.get("standby_guardian_email", ""),
+
+            "Name of Alternate guardian": data.get("alternate_guardian_name", ""),
+
+            "Name of Children 1": data.get("child1_name", ""),
+            "Date of Birth 1": data.get("child1_dob", ""),
+            "Name of Children 2": data.get("child2_name", ""),
+            "Date of Birth 2": data.get("child2_dob", ""),
+            "Name of Children 3": data.get("child3_name", ""),
+            "Date of Birth 3": data.get("child3_dob", ""),
+            "Name of Children 4": data.get("child4_name", ""),
+            "Date of Birth 4": data.get("child4_dob", ""),
+
+            "Name of Person with Parental Rights": data.get("other_parent_name", "NONE"),
+            "Relationship to Minor Child": data.get("other_parent_relationship", "N/A"),
+            "Name of Person with Parental Rights 2": data.get("other_parent_name_2", ""),
+            "Relationship to Minor child 2": data.get("other_parent_relationship_2", ""),
 
             "Box 3": data.get("guardian_person_limits", "NONE"),
             "Box 4": data.get("guardian_property_limits", "NONE"),
-
             "Location 1": data.get("child_property_location", "____"),
         }
 
@@ -37,7 +55,6 @@ def fill_standby_guardian():
         for page in writer.pages:
             writer.update_page_form_field_values(page, field_data)
 
-        # checkboxes based on your PDF fields
         checkbox_values = {
             "Check Box94": "/Yes",
             "Check Box97": "/Yes",
@@ -54,9 +71,8 @@ def fill_standby_guardian():
             "Check Box124": "/Yes",
         }
 
-        writer.update_page_form_field_values(writer.pages[0], checkbox_values)
-        writer.update_page_form_field_values(writer.pages[1], checkbox_values)
-        writer.update_page_form_field_values(writer.pages[2], checkbox_values)
+        for page in writer.pages:
+            writer.update_page_form_field_values(page, checkbox_values)
 
         output = tempfile.NamedTemporaryFile(delete=False, suffix=".pdf")
         output.close()
@@ -82,7 +98,7 @@ def list_fields():
     return jsonify(list(fields.keys()))
 
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
